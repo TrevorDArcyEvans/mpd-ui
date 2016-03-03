380c4077c3987b91229c764912e7c519f5f4c974
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2016-03-03 22:30:50 +0000

    - initial version of scrolling text

--- whatsplaying.py
+++ whatsplaying.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python
 import lcddriver
-from time import *
+import subprocess
+import time
 
 lcd = lcddriver.lcd()
 
-lcd.lcd_display_string("I love Mandy", 1)
-lcd.lcd_display_string("I love Ellie", 2)
+def ScrollDisplay(curr):
+  lcd.lcd_clear()
+  # nasty hack to simulate scrolling text
+  sep = " # "
+  dispStr = curr + sep + curr + sep + curr + sep + curr + sep
+  dispStr = dispStr + dispStr + dispStr
+  strLen = len(dispStr)
+  for i in range(0, strLen):
+    lcd.lcd_clear()
+    lcd.lcd_display_string(dispStr[i : i + 15], 1)
+    lcd.lcd_display_string(dispStr[i + 16 : i + 31], 2)
+    time.sleep(0.35)
 
+while True:
+  curr = subprocess.check_output(['mpc', 'current']).split('\n')
+  print curr[0]
+  ScrollDisplay(curr[0])
+  time.sleep(10)
