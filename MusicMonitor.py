5d96ddddee60f671272d75f7830bcc784de7f7a7
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2016-03-05 10:01:52 +0000

    - refactored - added support for ready LED

--- MusicMonitor.py
+++ MusicMonitor.py
@@ -5,19 +5,34 @@ from time import sleep
 
 import RPi.GPIO as GPIO
 
+# input buttons
+MpcPrev = 14
+MpcToggle = 15
+MpcNext = 16
+MpcReady = 17
+
+# disable warnings
+GPIO.setwarnings(False)
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(14, GPIO.IN)
-GPIO.setup(15, GPIO.IN)
-GPIO.setup(16, GPIO.IN)
+
+GPIO.setup(MpcPrev, GPIO.IN)
+GPIO.setup(MpcToggle, GPIO.IN)
+GPIO.setup(MpcNext, GPIO.IN)
+
+# turn on ready LED
+GPIO.setup(MpcReady, GPIO.OUT)
+sleep(1);
+GPIO.output(MpcReady, True)
 
 while True:
-    if (GPIO.input(14) == True):
+    if (GPIO.input(MpcPrev) == True):
         os.system('mpc prev &')
 
-    if (GPIO.input(15) == True):
+    if (GPIO.input(MpcToggle) == True):
         os.system('mpc toggle &')
 
-    if (GPIO.input(16)== True):
+    if (GPIO.input(MpcNext)== True):
         os.system('mpc next &')
 
     sleep(0.1);
